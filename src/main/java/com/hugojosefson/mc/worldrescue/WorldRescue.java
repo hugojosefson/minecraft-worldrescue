203c785d33ffe89895879cf9ddc9bd5ea5553c54
Hugo Josefson <hugo@josefson.org>
2021-02-28 19:11:01 +0100

    Remove comments on decompilation, because refactored.

--- src/main/java/com/hugojosefson/mc/worldrescue/WorldRescue.java
+++ src/main/java/com/hugojosefson/mc/worldrescue/WorldRescue.java
@@ -1,7 +1,3 @@
-//
-// Decompiled by Procyon v0.5.36
-//
-
 package com.hugojosefson.mc.worldrescue;
 
 import com.hugojosefson.mc.worldrescue.autosave.AutosaveRunnable;
--- src/main/java/com/hugojosefson/mc/worldrescue/commands/Commands.java
+++ src/main/java/com/hugojosefson/mc/worldrescue/commands/Commands.java
@@ -1,7 +1,3 @@
-//
-// Decompiled by Procyon v0.5.36
-//
-
 package com.hugojosefson.mc.worldrescue.commands;
 
 import com.hugojosefson.mc.worldrescue.WorldRescue;
